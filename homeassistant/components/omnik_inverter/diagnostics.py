6397efadd78bd678e6607fec325d38be6362bb71
Klaas Schoute <klaas_schoute@hotmail.com>
2022-01-27 17:33:55 +0100

    Add diagnostics support to Omnik Inverter

---
+++ homeassistant/components/omnik_inverter/diagnostics.py
@@ -0,0 +1,24 @@
+"""Diagnostics support for Omnik Inverter integration."""
+from __future__ import annotations
+
+from typing import Any
+
+from homeassistant.components.diagnostics import async_redact_data
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import CONF_HOST
+from homeassistant.core import HomeAssistant
+
+TO_REDACT = {CONF_HOST}
+
+
+async def async_get_config_entry_diagnostics(
+    hass: HomeAssistant, entry: ConfigEntry
+) -> dict[str, Any]:
+    """Return diagnostics for a config entry."""
+    return {
+        "entry": {
+            "title": entry.title,
+            "data": async_redact_data(entry.data, TO_REDACT),
+            "options": async_redact_data(entry.options, TO_REDACT),
+        },
+    }
