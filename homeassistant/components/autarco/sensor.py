029d6ec3d9eda3a7a179acd5d7be2d427b415699
Klaas Schoute <klaas_schoute@hotmail.com>
2022-03-05 16:56:16 +0100

    Creating inverter sensor entities

--- homeassistant/components/autarco/sensor.py
+++ homeassistant/components/autarco/sensor.py
@@ -6,17 +6,22 @@ from dataclasses import dataclass
 
 from homeassistant.components.sensor import (
     DOMAIN as SENSOR_DOMAIN,
+    SensorDeviceClass,
     SensorEntity,
     SensorEntityDescription,
 )
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import (
+    ENERGY_KILO_WATT_HOUR,
+    POWER_WATT,
+)
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.device_registry import DeviceEntryType
 from homeassistant.helpers.entity import DeviceInfo
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
-from . import AutarcoData, AutarcoDataUpdateCoordinator
+from .coordinator import AutarcoData, AutarcoDataUpdateCoordinator
 from .const import DOMAIN
 
 
@@ -47,15 +52,32 @@ SENSORS_ACCOUNT: tuple[AutarcoSensorEntityDescription, ...] = (
     ),
 )
 
+SENSORS_INVERTER: tuple[AutarcoSensorEntityDescription, ...] = (
+    AutarcoSensorEntityDescription(
+        key="serial_number",
+        name="Serial number",
+        value_fn=lambda inverters: inverters.values(),
+    ),
+    AutarcoSensorEntityDescription(
+        key="out_ac_power",
+        name="AC output power",
+        value_fn=lambda inverters: inverters.values(),
+    ),
+)
+
 SENSORS_SOLAR: tuple[AutarcoSensorEntityDescription, ...] = (
     AutarcoSensorEntityDescription(
         key="power_production",
         name="Power Production",
+        native_unit_of_measurement=POWER_WATT,
+        device_class=SensorDeviceClass.POWER,
         value_fn=lambda data: data.solar.power_production,
     ),
     AutarcoSensorEntityDescription(
         key="energy_production_today",
         name="Energy Production - Today",
+        native_unit_of_measurement=ENERGY_KILO_WATT_HOUR,
+        device_class=SensorDeviceClass.ENERGY,
         value_fn=lambda data: data.solar.energy_production_today,
     ),
 )
@@ -65,14 +87,37 @@ async def async_setup_entry(
     hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
 ) -> None:
     """Set up Autarco sensors based on a config entry."""
-    entities: list[AutarcoSensorEntity] = [
+    coordinator = hass.data[DOMAIN][entry.entry_id]
+    entities: list[AutarcoSensorEntity] = []
+    for index, inverter in enumerate(coordinator.data.inverters, start=1):
+        entities.extend(
+            AutarcoSensorEntity(
+                coordinator=coordinator,
+                description=description,
+                name=f"Inverter - {inverter.serial_number}",
+                service=f"inverter_{index}",
+            )
+            for description in SENSORS_INVERTER
+        )
+    entities.extend(
         AutarcoSensorEntity(
-            coordinator=hass.data[DOMAIN][entry.entry_id],
+            coordinator=coordinator,
             description=description,
+            name="Account",
+            service="account",
         )
         for description in SENSORS_ACCOUNT
-    ]
-
+    )
+    entities.extend(
+        AutarcoSensorEntity(
+            coordinator=coordinator,
+            description=description,
+            name="Solar",
+            service="solar",
+        )
+        for description in SENSORS_SOLAR
+    )
+    print(hass.data[DOMAIN][entry.entry_id].data)
     async_add_entities(entities)
 
 
@@ -87,19 +132,22 @@ class AutarcoSensorEntity(CoordinatorEntity[AutarcoData], SensorEntity):
         *,
         coordinator: AutarcoDataUpdateCoordinator,
         description: AutarcoSensorEntityDescription,
+        name: str,
+        service: str,
     ) -> None:
         """Initialize Autarco sensor."""
         super().__init__(coordinator=coordinator)
 
-        self.entity_id = f"{SENSOR_DOMAIN}.{description.key}"
         self.entity_description = description
+        self.entity_id = f"{SENSOR_DOMAIN}.{service}_{description.key}"
         self._attr_unique_id = (
-            f"{coordinator.data.account.public_key}_{description.key}"
+            f"{coordinator.config_entry.entry_id}_{service}_{description.key}"
         )
         self._attr_device_info = DeviceInfo(
-            identifiers={(DOMAIN, coordinator.data.account.public_key)},
+            identifiers={(DOMAIN, f"{coordinator.config_entry.entry_id}_{service}")},
             entry_type=DeviceEntryType.SERVICE,
             manufacturer="Autarco",
+            name=name,
         )
 
     @property
